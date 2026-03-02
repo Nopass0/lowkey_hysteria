e3b64765ccaa6ddcf15a28c25c92d3fe1cb09c59
Nopass0 <gal.bogdan2015@gmail.com>
2026-03-02 05:38:11 +0500

    Standardize Handshake: Revert to Array format for compact cross-lang compatibility

--- main.go
+++ main.go
@@ -213,6 +213,7 @@ func startHeartbeatDB() {
 // ---------------------------------------------------------
 
 type ClientHello struct {
+	_struct bool   `msgpack:",asArray"`
 	Auth    string `msgpack:"auth"`
 	Rx      uint64 `msgpack:"rx"`
 	Tx      uint64 `msgpack:"tx"`
@@ -220,11 +221,12 @@ type ClientHello struct {
 }
 
 type ServerHello struct {
-	Ok  bool   `msgpack:"ok"`
-	Msg string `msgpack:"msg"`
-	Id  uint32 `msgpack:"id"`
-	Rx  uint64 `msgpack:"rx"`
-	IP  string `msgpack:"ip"`
+	_struct bool   `msgpack:",asArray"`
+	Ok      bool   `msgpack:"ok"`
+	Msg     string `msgpack:"msg"`
+	Id      uint32 `msgpack:"id"`
+	Rx      uint64 `msgpack:"rx"`
+	IP      string `msgpack:"ip"`
 }
 
 type IPPool struct {
